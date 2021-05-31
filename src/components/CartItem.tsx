885f0d62c67ba3c7531819bd2fcb210143481a93
Okkar Min <omin001@e.ntu.edu.sg>
2021-05-31 18:09:33 +0800

    feat: :sparkles: Show toast when cart item is removed

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -2,7 +2,7 @@ import { useContext } from "react";
 
 import { CartContext } from "../context/CartContext";
 
-import { Box, Flex, Heading, Button, HStack } from "@chakra-ui/react";
+import { Box, Flex, Heading, Button, HStack, useToast } from "@chakra-ui/react";
 
 type Props = {
   cartItem: ICartItem;
@@ -11,8 +11,17 @@ type Props = {
 const CartItem: React.FC<Props> = ({ cartItem }) => {
   const { removeFromCart } = useContext(CartContext) as CartContextType;
 
+  const toast = useToast();
   const handleCartItemRemove = () => {
     removeFromCart(cartItem);
+
+    toast({
+      title: `${cartItem.shopItem.title} x ${cartItem.quantity} removed from cart`,
+      status: "warning",
+      position: "top-left",
+      duration: 1000,
+      isClosable: true,
+    });
   };
 
   return (
